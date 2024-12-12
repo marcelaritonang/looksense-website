30917220125f5f9078268425f8efa964d53c6201
Marcel Aritonang <62584017+marcelaritonang@users.noreply.github.com>
2024-12-13 03:10:33 +0700

    Deploy Looksense static build to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Ini menggantikan `next export`
-  basePath: '/looksense.github.io', // Sub-path untuk GitHub Pages
-  assetPrefix: '/looksense.github.io', // Prefix untuk semua aset
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
